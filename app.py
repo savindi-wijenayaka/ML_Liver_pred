8c2bc515ed282955c8294cd98d033e82760288d5
Savindi Wijenayaka <savindi.narmada@gmail.com>
2020-12-15 17:57:09 +0530

    reformatted code

--- app.py
+++ app.py
@@ -9,25 +9,25 @@ app = flask.Flask(__name__)
 CORS(app)
 
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
     data = {"success": False}
     input_json = request.json
     try:
         prob_of_disease = service(input_json)
 
-        if prob_of_disease>=0.5:
+        if prob_of_disease >= 0.5:
             # return render_template('form.html', pred=f'Your liver is in Danger.\nProbability of Liver disease is {prob_of_disease:.2f}')
-            return f'Your liver is in Danger.\nProbability of Liver disease is {prob_of_disease:.2f}'
+            return f"Your liver is in Danger.\nProbability of Liver disease is {prob_of_disease:.2f}"
         else:
             # return render_template('form.html',pred=f'Your liver is safe.\n Probability of Liver disease is {prob_of_disease:.2f}')
-            return f'Your liver is safe.\n Probability of Liver disease is {prob_of_disease:.2f}'
-        return 
+            return f"Your liver is safe.\n Probability of Liver disease is {prob_of_disease:.2f}"
+        return
     except Exception as exc:
         print("exception")
         data["prediction_Exception"] = str(exc)
         return data
-    
+
 
 # if this is the main thread of execution first load the model and
 @app.route("/")
--- main_logic.py
+++ main_logic.py
@@ -5,36 +5,45 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-scalling_cols = ['Age','Total_Bilirubin', 'Direct_Bilirubin', 'Alkaline_Phosphotase',
-       'Alamine_Aminotransferase', 'Aspartate_Aminotransferase',
-       'Total_Protiens', 'Albumin', 'Albumin_and_Globulin_Ratio']
+scalling_cols = [
+    "Age",
+    "Total_Bilirubin",
+    "Direct_Bilirubin",
+    "Alkaline_Phosphotase",
+    "Alamine_Aminotransferase",
+    "Aspartate_Aminotransferase",
+    "Total_Protiens",
+    "Albumin",
+    "Albumin_and_Globulin_Ratio",
+]
+
 
 def clean_data(data):
-  # Encode Gender
-  encoder=pickle.load(open('encoder.pkl','rb'))
-  fitted_values = encoder.transform(data[['Gender']])
-  enc_df = pd.DataFrame(fitted_values.toarray())
-  enc_df.rename(columns={0: 'Sex'}, inplace=True)
-  data = data.join(enc_df['Sex'])
-  data.drop(['Gender'], axis=1, inplace=True)
+    # Encode Gender
+    encoder = pickle.load(open("encoder.pkl", "rb"))
+    fitted_values = encoder.transform(data[["Gender"]])
+    enc_df = pd.DataFrame(fitted_values.toarray())
+    enc_df.rename(columns={0: "Sex"}, inplace=True)
+    data = data.join(enc_df["Sex"])
+    data.drop(["Gender"], axis=1, inplace=True)
 
-  # Impute missing values with mean
-  mean_values=pickle.load(open('mean_values.pkl','rb'))
-  data.fillna(mean_values, inplace=True)
+    # Impute missing values with mean
+    mean_values = pickle.load(open("mean_values.pkl", "rb"))
+    data.fillna(mean_values, inplace=True)
 
-  # Convert Sex field to int
-  data.Sex = data.Sex.astype(int)
+    # Convert Sex field to int
+    data.Sex = data.Sex.astype(int)
 
-  # Scale scalable fields with MinMaxScaler
-  min_max=pickle.load(open('scaler.pkl','rb'))
-  data[scalling_cols] = min_max.transform(data[scalling_cols])
+    # Scale scalable fields with MinMaxScaler
+    min_max = pickle.load(open("scaler.pkl", "rb"))
+    data[scalling_cols] = min_max.transform(data[scalling_cols])
 
-  return data
+    return data
 
 
 def service(input_json):
-  model_rfc=pickle.load(open('model_rfc.pkl','rb'))
-  sample_df = pd.DataFrame(input_json, index=[0])
-  sample_df = clean_data(sample_df)
-  prediction_prob = model_rfc.predict_proba(sample_df)
-  return prediction_prob[0][1]
+    model_rfc = pickle.load(open("model_rfc.pkl", "rb"))
+    sample_df = pd.DataFrame(input_json, index=[0])
+    sample_df = clean_data(sample_df)
+    prediction_prob = model_rfc.predict_proba(sample_df)
+    return prediction_prob[0][1]
